50cc55dc81be556d6559f685ff26d7133cddf0a4
Dominik Wuttke <Dominikwuttke@web.de>
2022-09-15 12:05:03 +0200

    - updated pyproject.toml - add sapl_base/__init__.py import framework packages of SAPL

---
+++ sapl_base/src/sapl_base/__init__.py
@@ -0,0 +1,29 @@
+import sys
+
+imported_addon = None
+
+try:
+    from sapl_django import *
+    imported_addon = 'sapl_django'
+except ImportError:
+    pass
+
+try:
+    from sapl_flask import *
+    if not imported_addon:
+        imported_addon = 'sapl_flask'
+    else:
+        sys.exit("%s is already imported, only one sapl_package can be installed" % imported_addon)
+except ImportError:
+    pass
+
+try:
+    from sapl_tornado import basic
+    if not imported_addon:
+        imported_addon = 'sapl_tornado'
+    else:
+        sys.exit("%s is already imported, only one sapl_package can be installed" % imported_addon)
+except ImportError:
+    pass
+
+del imported_addon
