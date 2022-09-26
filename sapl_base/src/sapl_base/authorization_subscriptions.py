947bff6af346cc60af6b945578fab9f5c9a6e346
Dominik Wuttke <Dominikwuttke@web.de>
2022-09-26 18:14:45 +0200

    - remove obsolete helper functions - add authorization_subscriptions.py documentation

--- sapl_base/src/sapl_base/authorization_subscriptions.py
+++ sapl_base/src/sapl_base/authorization_subscriptions.py
@@ -1,7 +1,5 @@
 import json
 
-import logging
-
 
 class AuthorizationSubscription:
     """
@@ -14,10 +12,14 @@ class AuthorizationSubscription:
         if not (isinstance(subscription_id, int) or subscription_id is None):
             raise TypeError(
                 f"subscription_id must be an int, was {subscription_id} type of {subscription_id.__class__}")
-        self.subject = subject
-        self.action = action
-        self.resource = resource
-        self.environment = environment
+        if subject is not None:
+            self.subject = subject
+        if action is not None:
+            self.action = action
+        if resource is not None:
+            self.resource = resource
+        if environment is not None:
+            self.environment = environment
         if subscription_id is not None:
             self.subscription_id = subscription_id
         else:
@@ -25,40 +27,33 @@ class AuthorizationSubscription:
 
     def __repr__(self):
         """
-        representation of python object AuthorizationSubscription,
-        usually eval will convert it back to that object
+        representation of an AuthorizationSubscription,
+        eval will convert it to an object
         """
-        dictionary = self._clean_dict()
+        dictionary = self.__dict__.copy()
         representative = ",".join(element + "=" + repr(dictionary.get(element)) for element in dictionary)
         return f"{type(self).__name__}({representative})"
 
-    def __eq__(self, other):
-        try:
-            return (self.subject == other.subject) and (
-                    self.action == other.action) and (
-                           self.resource == other.resource) and (
-                           self.environment == other.environment) and (
-                           self.subscription_id == other.subscription_id)
-        except AttributeError:
-            return False
-        except Exception as e:
-            logging.exception(e)
-            return False
+    # def __eq__(self, other):
+    #     try:
+    #         return (self.subject == other.subject) and (
+    #                 self.action == other.action) and (
+    #                        self.resource == other.resource) and (
+    #                        self.environment == other.environment) and (
+    #                        self.subscription_id == other.subscription_id)
+    #     except AttributeError:
+    #         return False
+    #     except Exception as e:
+    #         logging.exception(e)
+    #         return False
 
     def __str__(self):
         """
-        The Method __str__ gives back a json of the AuthorizationSubscription
+        Sting representation returns this object in json format as a string
         """
-        dictionary = self._clean_dict()
+        dictionary = self.__dict__.copy()
         dictionary.pop("subscription_id")
         return json.dumps(dictionary, indent=2, skipkeys=True, default=lambda o: str(o))
-
-    def _clean_dict(self):
-        dictionary = self.__dict__.copy()
-        for element in self.__dict__:
-            if self.__dict__.get(element) is None:
-                dictionary.pop(element)
-        return dictionary
 
 
 class MultiSubscription:
@@ -67,43 +62,40 @@ class MultiSubscription:
             environment=None,
             authorization_subscriptions=None,
     ):
+        if subject is not None:
+            self.subject = subject
+        if action is not None:
+            self.action = action
+        if resource is not None:
+            self.resource = resource
+        if environment is not None:
+            self.environment = environment
 
-        self.subject = subject
-        self.action = action
-        self.resource = resource
-        self.environment = environment
         self.authorization_subscriptions = authorization_subscriptions
 
     def __repr__(self):
         """
-        representation of python object AuthorizationSubscription,
-        usually eval will convert it back to that object
+        representation of an AuthorizationSubscription,
+        eval will convert it to an object
         """
-        dictionary = self.__dict__
-        for element in dictionary:
-            if element is None: del element
-        representative = ",".join(element + "=" + repr(dictionary.get(element)) for element in dictionary)
+        representative = ",".join(element + "=" + repr(self.__dict__.get(element)) for element in self.__dict__)
         return f"{type(self).__name__}({representative})"
 
     def __str__(self):
         """
-        The Method __str__ gives back a json of the AuthorizationSubscription
+        Sting representation returns this object in json format as a string
         """
-        dictionary = self.__dict__.copy()
-        for element in self.__dict__:
-            if self.__dict__.get(element) is None:
-                dictionary.pop(element)
-        return json.dumps(dictionary, indent=2, skipkeys=True, default=lambda o: str(o))
+        return json.dumps(self.__dict__, indent=2, skipkeys=True, default=lambda o: str(o))
 
-    def __eq__(self, other):
-        try:
-            return (self.subject == other.subject) and (
-                    self.action == other.action) and (
-                           self.resource == other.resource) and (
-                           self.environment == other.environment) and (
-                           self.authorization_subscriptions == other.authorization_subscriptions)
-        except AttributeError:
-            return False
-        except Exception as e:
-            logging.exception(e)
-            return False
+    # def __eq__(self, other):
+    #     try:
+    #         return (self.subject == other.subject) and (
+    #                 self.action == other.action) and (
+    #                        self.resource == other.resource) and (
+    #                        self.environment == other.environment) and (
+    #                        self.authorization_subscriptions == other.authorization_subscriptions)
+    #     except AttributeError:
+    #         return False
+    #     except Exception as e:
+    #         logging.exception(e)
+    #         return False
