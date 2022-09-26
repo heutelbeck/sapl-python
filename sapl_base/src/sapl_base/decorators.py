f8e687238a216ceaea2250a31c3a86401fcf9c16
Dominik Wuttke <Dominikwuttke@web.de>
2022-09-26 23:56:20 +0200

    - add decorators.py documentation - add policy_decision_points.py documentation

--- sapl_base/src/sapl_base/decorators.py
+++ sapl_base/src/sapl_base/decorators.py
@@ -1,43 +1,71 @@
 import asyncio
 from functools import wraps
 
-from sapl_base.policy_enforcement_point import BasePolicyEnforcementPoint
-from sapl_base.sapl_util import double_wrap, get_named_args_dict
+from sapl_base.policy_enforcement_point import PolicyEnforcementPoint
+from sapl_base.sapl_util import double_wrap
 
 
 @double_wrap
-def pre_enforce(fn, subject: str = None, action: str = None, resource: str = None, environment=None, scope="basic"):
+def pre_enforce(fn, subject: str | () = None, action: str | () = None, resource: str | () = None,
+                environment: str | () = None):
+    """
+    Pre_enforces a decorated function with SAPL.
+    If the function returns a stream, pre_enforce will cancel the stream when a DENY is received.
+
+    Identify if the function is a coroutine and calls the appropriate function to handle the enforcement in a coroutine
+    throws an exception, when the decorated function returns a stream and is no coroutine.
+
+    :param fn:  decorated function
+    :param subject: subject of an authorization_subscription or a function to create the subject
+    :param action: action of an authorization_subscription or a function to create the action
+    :param resource: resource of an authorization_subscription or a function to create the resource
+    :param environment: environment of an authorization_subscription or a function to create the environment
+    :return: The pre_enforced value of the decorated function
+    """
     if asyncio.iscoroutinefunction(fn):
         @wraps(fn)
         async def wrap(*args, **kwargs):
-            enforcement_point = BasePolicyEnforcementPoint(fn, args, kwargs)
-            return await enforcement_point.pre_enforce(subject, action, resource, environment, scope)
+            enforcement_point = PolicyEnforcementPoint(fn, args, kwargs)
+            return await enforcement_point.pre_enforce(subject, action, resource, environment)
 
         return wrap
     else:
         @wraps(fn)
         def sync_wrap(*args, **kwargs):
-            enforcement_point = BasePolicyEnforcementPoint(fn, args, kwargs)
-            return enforcement_point.sync_pre_enforce(subject, action, resource, environment, scope)
+            enforcement_point = PolicyEnforcementPoint(fn, args, kwargs)
+            return enforcement_point.sync_pre_enforce(subject, action, resource, environment)
 
         return sync_wrap
 
 
 @double_wrap
-def post_enforce(fn, subject: str = None, action: str = None, resource: str = None, environment=None, scope="basic"):
+def post_enforce(fn, subject: str | () = None, action: str | () = None, resource: str | () = None,
+                 environment: str | () = None):
+    """
+    Post_enforces a decorated function with SAPL.
+    If the function returns a stream an exception is thrown.
+    Identify if the function is a coroutine and calls the appropriate function to handle the enforcement in a coroutine
+
+    :param fn:  decorated function
+    :param subject: subject of an authorization_subscription or a function to create the subject
+    :param action: action of an authorization_subscription or a function to create the action
+    :param resource: resource of an authorization_subscription or a function to create the resource
+    :param environment: environment of an authorization_subscription or a function to create the environment
+    :return: The post_enforced value of the decorated function
+    """
     if asyncio.iscoroutinefunction(fn):
         @wraps(fn)
         async def wrap(*args, **kwargs):
-            enforcement_point = BasePolicyEnforcementPoint(fn, *args, **kwargs)
-            return await enforcement_point.post_enforce(subject, action, resource, environment, scope)
+            enforcement_point = PolicyEnforcementPoint(fn, *args, **kwargs)
+            return await enforcement_point.post_enforce(subject, action, resource, environment)
 
         return wrap
     else:
         @wraps(fn)
         def sync_wrap(*args, **kwargs):
 
-            enforcement_point = BasePolicyEnforcementPoint(fn, *args, **kwargs)
-            return enforcement_point.sync_post_enforce(subject, action, resource, environment, scope)
+            enforcement_point = PolicyEnforcementPoint(fn, *args, **kwargs)
+            return enforcement_point.sync_post_enforce(subject, action, resource, environment)
 
         return sync_wrap
 
@@ -45,53 +73,101 @@ def post_enforce(fn, subject: str = None, action: str = None, resource: str = No
 """
 SAPL Decorators must be used as first Decorator to gather needed information of the annotated Function.
 """
+
+
 @double_wrap
-def pre_and_post_enforce(fn, subject: str = None, action: str = None, resource: str = None, environment=None):
+def pre_and_post_enforce(fn, subject: str | () = None, action: str | () = None, resource: str | () = None,
+                         environment: str | () = None):
+    """
+    Pre- and post_enforces a decorated function with SAPL.
+
+    If the function returns a stream an exception is thrown.
+
+    Identify if the function is a coroutine and calls the appropriate function to handle the enforcement in a coroutine
+
+    :param fn:  decorated function
+    :param subject: subject of an authorization_subscription or a function to create the subject
+    :param action: action of an authorization_subscription or a function to create the action
+    :param resource: resource of an authorization_subscription or a function to create the resource
+    :param environment: environment of an authorization_subscription or a function to create the environment
+    :return: The pre- and post_enforced value of the decorated function
+    """
     if asyncio.iscoroutinefunction(fn):
         @wraps(fn)
         async def wrap(*args, **kwargs):
-            enforcement_point = BasePolicyEnforcementPoint(fn, *args, **kwargs)
-            return await enforcement_point.pre_and_post_enforce(subject, action, resource, environment, scope)
+            enforcement_point = PolicyEnforcementPoint(fn, *args, **kwargs)
+            return await enforcement_point.pre_and_post_enforce(subject, action, resource, environment)
 
         return wrap
     else:
         @wraps(fn)
         def sync_wrap(*args, **kwargs):
-            enforcement_point = BasePolicyEnforcementPoint(fn, *args, **kwargs)
-            return enforcement_point.sync_pre_and_post_enforce(subject, action, resource, environment, scope)
+            enforcement_point = PolicyEnforcementPoint(fn, *args, **kwargs)
+            return enforcement_point.sync_pre_and_post_enforce(subject, action, resource, environment)
 
         return sync_wrap
 
 
 @double_wrap
-def enforce_drop_while_denied(fn, subject: str = None, action: str = None, resource: str = None, environment=None,
-                              scope="basic"):
+def enforce_drop_while_denied(fn, subject: str | () = None, action: str | () = None, resource: str | () = None,
+                              environment: str | () = None):
+    """
+    Enforces a stream and drops values, when the current decision is not PERMIT
+
+    If the function doesn't return a stream, an exception will be thrown
+
+    Identify if the function is a coroutine and will throw an exception, if it is not a coroutine.
+
+    :param fn:  decorated function
+    :param subject: subject of an authorization_subscription or a function to create the subject
+    :param action: action of an authorization_subscription or a function to create the action
+    :param resource: resource of an authorization_subscription or a function to create the resource
+    :param environment: environment of an authorization_subscription or a function to create the environment
+    :return: The return_value of the decorated function with a generator which will enforce each value, which is sent with the stream
+    """
     if asyncio.iscoroutinefunction(fn):
         @wraps(fn)
         async def wrap(*args, **kwargs):
-            enforcement_point = BasePolicyEnforcementPoint(fn, args, kwargs)
-            return await enforcement_point.drop_while_denied(subject, action, resource, environment, scope)
+            enforcement_point = PolicyEnforcementPoint(fn, args, kwargs)
+            return await enforcement_point.drop_while_denied(subject, action, resource, environment)
 
         return wrap
     else:
         @wraps(fn)
         def sync_wrap(*args, **kwargs):
             raise Exception
+
         return sync_wrap
 
 
 @double_wrap
-def enforce_recoverable_if_denied(fn, subject: str = None, action: str = None, resource: str = None, environment=None,
-                                  scope: str = "basic"):
+def enforce_recoverable_if_denied(fn, subject: str | () = None, action: str | () = None, resource: str | () = None,
+                                  environment: str | () = None):
+    """
+    Enforces a stream and drops values, when the current decision is not PERMIT and notifys the client, that
+    values are dropped, because they are not PERMITTED to receive them.
+
+    If the function doesn't return a stream, an exception will be thrown
+
+    Identify if the function is a coroutine and will throw an exception, if it is not a coroutine.
+
+    :param fn:  decorated function
+    :param subject: subject of an authorization_subscription or a function to create the subject
+    :param action: action of an authorization_subscription or a function to create the action
+    :param resource: resource of an authorization_subscription or a function to create the resource
+    :param environment: environment of an authorization_subscription or a function to create the environment
+    :return: The return_value of the decorated function with a generator which will enforce each value, which is sent with the stream
+    """
     if asyncio.iscoroutinefunction(fn):
         @wraps(fn)
         async def wrap(*args, **kwargs):
-            enforcement_point = BasePolicyEnforcementPoint(fn, args, kwargs)
-            return await enforcement_point.recoverable_if_denied(subject, action, resource, environment, scope)
+            enforcement_point = PolicyEnforcementPoint(fn, args, kwargs)
+            return await enforcement_point.recoverable_if_denied(subject, action, resource, environment)
 
         return wrap
     else:
         @wraps(fn)
         def sync_wrap(*args, **kwargs):
             raise Exception
+
         return sync_wrap
--- sapl_base/src/sapl_base/policy_decision_points.py
+++ sapl_base/src/sapl_base/policy_decision_points.py
@@ -1,4 +1,3 @@
-import logging
 from abc import ABC, abstractmethod
 from base64 import b64encode
 
@@ -10,50 +9,78 @@ from sapl_base.authorization_subscriptions import AuthorizationSubscription
 
 class PolicyDecisionPoint(ABC):
 
-    # Determine how to get a Remote PDP from settings independent from the Framework
-    # Maybe with a toml file
+    # TODO: read how to get data from pyproject.toml file ( look at lib/site-packages/_pytest/config/findpaths.py )
+    # TODO: get configuration for from_settings(cls) from pyproject.toml
+
     @classmethod
     def from_settings(cls):
+        """
+        reads the configuration in the pyproject.toml file and creates a PolicyDecisionPoint depending on the configuration
+        """
+        # TODO: read config from .toml file and decide which PDP
         return RemotePolicyDecisionPoint()
 
     @classmethod
     def dummy_pdp(cls):
+        """
+        :returns a DummyPolicyDecisionPoint, which always returns a 'PERMIT'
+        """
         return DummyPolicyDecisionPoint()
 
     @abstractmethod
     async def decide(self, subscription, decision_events="decide"):
+        """
+        async function to make a request to a pdp with the given subscription and event to create a stream of decisions
+
+        :param subscription: authorization_subscription which will be sent to a pdp to receive decisions based on it
+        :param decision_events: what kind of decision will be requested from the pdp. defaults to 'decide'
+        """
         pass
 
     @abstractmethod
     async def decide_once(self, subscription: AuthorizationSubscription, decision_events="decide"):
+        """
+        async function to make a request to a pdp with the given subscription and event to receive a
+        single decision
+
+        :param subscription: authorization_subscription which will be sent to a pdp to receive decisions based on it
+        :param decision_events: what kind of decision will be requested from the pdp. defaults to 'decide'
+        """
         pass
 
     @abstractmethod
     def sync_decide(self, subscription: AuthorizationSubscription, decision_events="decide"):
+        """
+        synchronous function to make a request to a pdp with the given subscription and event to receive a
+        single decision
+
+        :param subscription: authorization_subscription which will be sent to a pdp to receive decisions based on it
+        :param decision_events: what kind of decision will be requested from the pdp. defaults to 'decide'
+        """
         pass
 
-    @abstractmethod
-    def sync_decide_once(self, subscription: AuthorizationSubscription, decision_events="decide"):
-        pass
+    # @abstractmethod
+    # def sync_decide_once(self, subscription: AuthorizationSubscription, decision_events="decide"):
+    #    pass
 
 
 class DummyPolicyDecisionPoint(PolicyDecisionPoint):
     """
-    Dummy settings to run the application without SAPL-Remote-Server, all will be PREMIT in this case
+    Dummy settings to run the application without SAPL-Remote-Server, all will be PERMIT in this case
     """
 
     def __init__(self):
-        super(DummyPolicyDecisionPoint, self).__init__(dummy=True)
-        self.logger = logging.getLogger(__name__)
-        self.logger.warning(
-            "ATTENTION THE APPLICATION USES A DUMMY PDP. ALL AUTHORIZATION REQUEST WILL RESULT IN A SINGLE "
-            "PERMIT DECISION. DO NOT USE THIS IN PRODUCTION! THIS IS A PDP FOR TESTING AND DEVELOPING "
-            "PURPOSE ONLY!"
-        )
+        super(DummyPolicyDecisionPoint, self).__init__()
+        # self.logger = logging.getLogger(__name__)
+        # self.logger.warning(
+        #     "ATTENTION THE APPLICATION USES A DUMMY PDP. ALL AUTHORIZATION REQUEST WILL RESULT IN A SINGLE "
+        #     "PERMIT DECISION. DO NOT USE THIS IN PRODUCTION! THIS IS A PDP FOR TESTING AND DEVELOPING "
+        #     "PURPOSE ONLY!"
+        # )
 
     async def decide(self, subscription, decision_events="decide"):
         """
-        The method give back always PERMIT for the interface decide
+        implementation of decide, which always yields a PERMIT
         """
 
         yield {"decision": "PERMIT"}
@@ -62,31 +89,18 @@ class DummyPolicyDecisionPoint(PolicyDecisionPoint):
             self, subscription: AuthorizationSubscription, decision_events="decide"
     ):
         """
-        The Method give back always PERMIT for the Interface decide_one
+        implementation of decide_once, which always returns a PERMIT
         """
         return {"decision": "PERMIT"}
 
     def sync_decide(self, subscription: AuthorizationSubscription, decision_events="decide"):
         """
-        The method give back always PERMIT for the interface decide
-        """
-
-        yield {"decision": "PERMIT"}
-
-    def sync_decide_once(self, subscription: AuthorizationSubscription, decision_events="decide"):
-        """
-        The Method give back always PERMIT for the Interface decide_one
+        implementation of sync_decide, which always returns a PERMIT
         """
         return {"decision": "PERMIT"}
 
 
 class RemotePolicyDecisionPoint(PolicyDecisionPoint, ABC):
-    # DEFAULT_POLICY_DECISION_POINT_SETTINGS = {
-    #    "base_url": "http://localhost:8443/api/pdp/",
-    #    "key": "YJidgyT2mfdkbmL",
-    #    "secret": "Fa4zvYQdiwHZVXh",
-    #    "verify": False,
-    # }
     headers = {"Content-type": "application/json"}
 
     def __init__(self, base_url="http://localhost:8443/api/pdp/",
@@ -99,26 +113,40 @@ class RemotePolicyDecisionPoint(PolicyDecisionPoint, ABC):
             key_and_secret = b64encode(str.encode(f"{key}:{secret}")).decode("ascii")
             self.headers["Authorization"] = f"Basic {key_and_secret}"
 
-    def _sync_request(self, subscription, decision_events):
-        stream_response = requests.post(
-            self.base_url + decision_events,
-            subscription,
-            stream=True,
-            verify=self.verify,
-            headers=self.headers
-        )
-        return stream_response
+    # def _sync_request(self, subscription, decision_events):
+    #     stream_response = requests.post(
+    #         self.base_url + decision_events,
+    #         subscription,
+    #         stream=True,
+    #         verify=self.verify,
+    #         headers=self.headers
+    #     )
+    #     return stream_response
 
     def sync_decide(self, subscription: AuthorizationSubscription,
-                     decision_events="decide"):
-        with self._sync_request(
-                subscription, decision_events
+                    decision_events="decide"):
+        """
+        Makes a synchronous request to the set pdp and returns a decision for the sent authorization_subscription
+
+        :param subscription: An Authorization_Subscription which will be sent to the set pdp to receive decisions
+        :param decision_events: Signal to tell the PDP what kind of Authorization_Subscription is sent
+        and what kind of decision is expected
+        """
+        with requests.post(
+                self.base_url + decision_events,
+                subscription,
+                stream=True,
+                verify=self.verify,
+                headers=self.headers
         ) as stream_response:
+            # with self._sync_request(
+            #         subscription, decision_events
+            # ) as stream_response:
             if stream_response.status_code == 204:
-                return {"decision": "INDETERMINATE"}
+                return {"decision": "DENY"}
                 # return
             elif stream_response.status_code != 200:
-                return {"decision": "INDETERMINATE"}
+                return {"decision": "DENY"}
                 # return
             else:
                 lines = b''
@@ -129,21 +157,27 @@ class RemotePolicyDecisionPoint(PolicyDecisionPoint, ABC):
                         response = ''
                         for item in line_set:
                             response += item.decode('utf-8')
-                        return response
+                        if response == {"decision": "INDETERMINATE"}:
+                            return {"decision": "DENY"}
 
-    def sync_decide_once(self, subscription: AuthorizationSubscription, decision_events="decide"
-                         ):
-        decision = self.sync_decide(subscription, decision_events)
-        if decision == {"decision": "INDETERMINATE"}:
-            return {"decision": "DENY"}
-        return decision
+    # def sync_decide_once(self, subscription: AuthorizationSubscription, decision_events="decide"):
+    #    decision = self.sync_decide(subscription, decision_events)
+    #    if decision == {"decision": "INDETERMINATE"}:
+    #        return {"decision": "DENY"}
+    #    return decision
 
     async def decide(self, subscription, decision_events="decide"):
+        """
+        Makes a request to the set pdp and returns a stream of decisions for the sent authorization_subscription
 
+        :param subscription: An Authorization_Subscription which will be sent to the set pdp to receive decisions
+        :param decision_events: Signal to tell the PDP what kind of Authorization_Subscription is sent
+        and what kind of decisions is expected
+        """
         async with aiohttp.ClientSession(raise_for_status=True
                                          ) as session:
-            async with session.get(
-                    'http://localhost:8080/') as response:  # self.base_url + decision_events, data=subscription, verify_ssl=self.verify,
+            async with session.get('http://localhost:8080/') as response:
+                # self.base_url + decision_events, data=subscription, verify_ssl=self.verify,
                 # headers=self.headers)
                 if response.status == 204:
                     yield {"decision": "INDETERMINATE"}
@@ -166,6 +200,14 @@ class RemotePolicyDecisionPoint(PolicyDecisionPoint, ABC):
     async def decide_once(
             self, subscription: AuthorizationSubscription, decision_events="decide"
     ):
+        """
+        Makes a request to the set pdp and returns the first decision received for the sent authorization_subscription
+
+        :param subscription: An Authorization_Subscription which will be sent to the set pdp to receive a decision
+        :param decision_events: Signal to tell the PDP what kind of Authorization_Subscription is sent
+        and what kind of decision is expected
+        :return: A decision for the given Authorization_Subscription
+        """
         decision_stream = self.decide(subscription, decision_events)
         try:
             decision = await decision_stream.__anext__()
