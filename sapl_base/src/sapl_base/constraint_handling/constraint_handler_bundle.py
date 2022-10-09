4ed07583a0d0c1f6fc2e7d112d95f606aa377aa1
Dominik Wuttke <Dominikwuttke@web.de>
2022-10-09 16:14:27 +0200

    - refactor constraint_handler_bundle.py ConstraintHandlerProviders are replaced by a list of functions - add constraint_handler_service.py functions to create ConstraintHandlerBundles

--- sapl_base/src/sapl_base/constraint_handling/constraint_handler_bundle.py
+++ sapl_base/src/sapl_base/constraint_handling/constraint_handler_bundle.py
@@ -1,35 +1,39 @@
-from typing import Any
-
-from sapl_base.constraint_handling.constraint_handler_provider import OnDecisionConstraintHandlerProvider, \
-    ErrorConstraintHandlerProvider, ResultConstraintHandlerProvider, FunctionArgumentsConstraintHandlerProvider
+from typing import Any, Callable, Union
 
 
 class ConstraintHandlerBundle:
     # TODO Use a Dictionary instead of an array to distinguish which handler shall be called at what stage
     # TODO async handling of Constraints
-    _on_decision_handler: list[OnDecisionConstraintHandlerProvider] = []
-    _error_handler: list[ErrorConstraintHandlerProvider] = []
-    _result_handler: list[ResultConstraintHandlerProvider] = []
+    _on_decision_handler: list[Callable[[Any], None]]  # Change to decision
+    _error_handler: list[Callable[[Exception], None]]
+    _result_handler: list[Callable[[Any], Any]]
+    _function_arguments_mapper: list[Callable[[dict], None]]
+
+    def __init__(self, on_decision_handler, error_handler, result_handler,
+                 function_arguments_mapper: Union[list[Callable[[dict], None]], Any] = None):
+
+        if function_arguments_mapper is None:
+            self._function_arguments_mapper = []
+        else:
+            self._function_arguments_mapper = function_arguments_mapper
+        self._on_decision_handler = on_decision_handler
+        self._result_handler = result_handler
+        self._error_handler = error_handler
 
     def execute_on_decision_handler(self, decision):
         for handler in self._on_decision_handler:
-            handler.handle(decision)
+            handler(decision)
 
     def execute_on_error_handler(self, exception: Exception):
         for handler in self._error_handler:
-            handler.handle(exception)
+            handler(exception)
 
     def execute_result_handler(self, result: Any) -> Any:
         current_result = result
         for handler in self._result_handler:
-            current_result = handler.handle(result)
+            current_result = handler(current_result)
         return current_result
-
-
-class PreEnforceConstraintHandlerBundle(ConstraintHandlerBundle):
-
-    _function_arguments_mapper: list[FunctionArgumentsConstraintHandlerProvider] = []
 
     def execute_function_arguments_mapper(self, arguments: dict) -> None:
         for handler in self._function_arguments_mapper:
-            handler.handle(arguments)
+            handler(arguments)
--- sapl_base/src/sapl_base/constraint_handling/constraint_handler_service.py
+++ sapl_base/src/sapl_base/constraint_handling/constraint_handler_service.py
@@ -1,5 +1,9 @@
+from typing import Any, Callable, Union
+
+from sapl_base.constraint_handling.constraint_handler_bundle import ConstraintHandlerBundle
 from sapl_base.constraint_handling.constraint_handler_provider import OnDecisionConstraintHandlerProvider, \
-    ErrorConstraintHandlerProvider, FunctionArgumentsConstraintHandlerProvider, ResultConstraintHandlerProvider
+    ErrorConstraintHandlerProvider, FunctionArgumentsConstraintHandlerProvider, ResultConstraintHandlerProvider, \
+    ConstraintHandlerProvider
 
 
 class ConstraintHandlerService:
@@ -14,14 +18,108 @@ class ConstraintHandlerService:
     def __init__(self):
         pass
 
-    def build_post_enforce_bundle(self):
-        pass
+    def build_post_enforce_bundle(self, decision):
+        obligations, advices = self._get_obligations_and_advices(decision)
+        unhandled_obligations = []
+        for obligation in obligations:
+            unhandled_obligations.append(obligation)
+        on_decision_handler, error_handler, result_handler = self._build_basic_bundle(obligations, advices,
+                                                                                      unhandled_obligations)
 
-    def build_pre_enforce_bundle(self):
-        pass
+        if unhandled_obligations is not None:
+            raise Exception
+        return ConstraintHandlerBundle(on_decision_handler, error_handler, result_handler)
 
-    def _get_responsible_handler(self, constraints):
-        pass
+    def build_pre_enforce_bundle(self, decision):
+        obligations, advices = self._get_obligations_and_advices(decision)
+        unhandled_obligations = []
+        for obligation in obligations:
+            unhandled_obligations.append(obligation)
+        on_decision_handler, error_handler, result_handler = self._build_basic_bundle(obligations, advices,
+                                                                                      unhandled_obligations)
+
+        function_arguments_mapper = self._create_function_argument_mapper(obligations, unhandled_obligations)
+        function_arguments_mapper.extend(self._create_function_argument_mapper(advices))
+        if unhandled_obligations is not None:
+            raise Exception
+        ConstraintHandlerBundle(on_decision_handler, error_handler, result_handler, function_arguments_mapper)
+
+    def _build_basic_bundle(self, obligations, advices, unhandled_obligations):
+        on_decision_handler = self._create_on_decision_handler(obligations, unhandled_obligations)
+        on_decision_handler.extend(self._create_on_decision_handler(advices))
+        error_handler = self._create_on_error_handler(obligations, unhandled_obligations)
+        error_handler.extend(self._create_on_error_handler(advices))
+        result_handler = self._create_result_handler(obligations, unhandled_obligations)
+        result_handler.extend(self._create_result_handler(advices))
+        return on_decision_handler, error_handler, result_handler
+
+    def _create_on_decision_handler(self, constraints: list, unhandled_obligations: Union[list, None] = None) -> \
+            list[Callable[[Any], None]]:
+        if unhandled_obligations is None:
+            unhandled_obligations = []
+        handler_list = []
+        for constraint in constraints:
+            for handler in self._on_decision_handler:
+                self._add_responsible_handler(handler, constraint, handler_list, unhandled_obligations)
+        handler_list.sort(key=lambda provider: provider.priority())
+        return handler_list
+
+    def _create_on_error_handler(self, constraints: list, unhandled_obligations: Union[list, None] = None) -> \
+            list[Callable[[Exception], None]]:
+        if unhandled_obligations is None:
+            unhandled_obligations = []
+        handler_list = []
+        for constraint in constraints:
+            for handler in self._error_handler:
+                self._add_responsible_handler(handler, constraint, handler_list, unhandled_obligations)
+        handler_list.sort(key=lambda provider: provider.priority())
+        return handler_list
+
+    def _create_result_handler(self, constraints: list, unhandled_obligations: Union[list, None] = None) -> \
+            list[Callable[[Any], Any]]:
+        if unhandled_obligations is None:
+            unhandled_obligations = []
+        handler_list = []
+        for constraint in constraints:
+            for handler in self._result_handler:
+                self._add_responsible_handler(handler, constraint, handler_list, unhandled_obligations)
+        handler_list.sort(key=lambda provider: provider.priority())
+        return handler_list
+
+    def _create_function_argument_mapper(self, constraints: list, unhandled_obligations: Union[list, None] = None) -> \
+            list[Callable[[dict], None]]:
+        if unhandled_obligations is None:
+            unhandled_obligations = []
+        handler_list = []
+        for constraint in constraints:
+            for handler in self._function_arguments_mapper:
+                self._add_responsible_handler(handler, constraint, handler_list, unhandled_obligations)
+        handler_list.sort(key=lambda provider: provider.priority())
+        return handler_list
+
+    @staticmethod
+    def _add_responsible_handler(handler: ConstraintHandlerProvider, constraint, handler_list: list,
+                                 unhandled_obligations: list):
+        if handler.is_responsible(constraint):
+            handler_list.append(handler)
+            try:
+                unhandled_obligations.remove(constraint)
+            except ValueError:
+                pass
+
+    @staticmethod
+    def _get_obligations_and_advices(decision):
+        obligations = []
+        advices = []
+        if hasattr(decision, "obligation"):
+            obligation = getattr(decision, "obligation")
+            for item in obligation:
+                obligations.append(item)
+        if hasattr(decision, "advices"):
+            advice = getattr(decision, "advices")
+            for item in advice:
+                advices.append(item)
+        return obligations, advices
 
 
 constraint_handler_service = ConstraintHandlerService()
