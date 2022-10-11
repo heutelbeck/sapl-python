37c96732fe368e109c73278020049382145ede76
Dominik Wuttke <Dominikwuttke@web.de>
2022-10-11 10:22:52 +0200

    - fix missing dependency for tests - fix typo in test_policy_decision_points.py

--- sapl_base/tests/test_policy_decision_points.py
+++ sapl_base/tests/test_policy_decision_points.py
@@ -13,23 +13,23 @@ class TestDummyPdp:
         assert isinstance(dummy_pdp, DummyPolicyDecisionPoint)
 
     def test_decide_returns_permit(self, dummy_pdp):
-        assert (dummy_pdp.sync_decide(None), {"decision", "PERMIT"})
+        assert dummy_pdp.sync_decide(None) == {"decision": "PERMIT"}
 
     @pytest.mark.asyncio
     async def test_async_decide_once_returns_permit(self, event_loop, dummy_pdp):
         decision = await dummy_pdp.decide_once(None)
-        assert (decision, {"decision", "PERMIT"})
+        assert decision == {"decision": "PERMIT"}
 
     @pytest.mark.asyncio
     async def test_async_decide_yields_permit(self, event_loop, dummy_pdp):
         async def decision_collector_gen():
             while True:
                 decision = yield
-                assert (decision, {"decision", "PERMIT"})
+                assert decision == {"decision": "PERMIT"}
 
         collector_gen = decision_collector_gen()
         await collector_gen.asend(None)
         initial_decision, permit_stream = await dummy_pdp.decide(None, collector_gen)
-        assert (initial_decision, {"decision": "PERMIT"})
+        assert initial_decision == {"decision": "PERMIT"}
         await permit_stream
 
