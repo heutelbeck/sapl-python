f04db56d638afaede10c0fa8b9633969bcf89f6d
Dominik Wuttke <Dominikwuttke@web.de>
2023-01-31 16:09:42 +0100

    - fix error on missing configuration entry

--- sapl_django/src/sapl_django/__init__.py
+++ sapl_django/src/sapl_django/__init__.py
@@ -1,12 +1,17 @@
 import importlib
+import logging
 
 from django.core.exceptions import PermissionDenied
 from django.conf import settings
 import sapl_base.policy_decision_points
 
-if settings.POLICY_DECISION_POINT:
-    pdp_config = settings.POLICY_DECISION_POINT
-else:
+try:
+    if settings.POLICY_DECISION_POINT:
+        pdp_config = settings.POLICY_DECISION_POINT
+    else:
+        pdp_config = {}
+except AttributeError as e:
+    logging.info("Using default PDP configuration, because configuration is missing POLICY_DECISION_POINT entry")
     pdp_config = {}
 
 sapl_base.policy_decision_points.pdp = sapl_base.policy_decision_points.PolicyDecisionPoint.from_settings(pdp_config)
